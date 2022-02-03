cf11fa3366a053ffe652750c4d76933a891d47d6
fasonju <gimpact709@gmail.com>
2022-02-03 14:00:45 +0100

    first commit

---
+++ playersturn.js
@@ -0,0 +1,75 @@
+var order = []
+var counter = 0
+var snd2 = new Audio("beep-03.mp3")
+
+function beep2()
+{
+    snd2.play()
+}
+function playersturn()
+{
+    if(counter == 0){
+        order = curr.slice()
+    }
+    currentcolor = order.shift()
+    switch(currentcolor){
+        case 0:
+            document.getElementById("one").addEventListener("click",correctnode)
+            document.getElementById("two").addEventListener("click",wrongnode)
+            document.getElementById("three").addEventListener("click",wrongnode)
+            document.getElementById("four").addEventListener("click",wrongnode)
+            break
+        case 1:
+            document.getElementById("one").addEventListener("click",wrongnode)
+            document.getElementById("two").addEventListener("click",correctnode)
+            document.getElementById("three").addEventListener("click",wrongnode)
+            document.getElementById("four").addEventListener("click",wrongnode)
+            break
+        case 2:
+            document.getElementById("one").addEventListener("click",wrongnode)
+            document.getElementById("two").addEventListener("click",wrongnode)
+            document.getElementById("three").addEventListener("click",correctnode)
+            document.getElementById("four").addEventListener("click",wrongnode)
+            break
+        case 3:
+            document.getElementById("one").addEventListener("click",wrongnode)
+            document.getElementById("two").addEventListener("click",wrongnode)
+            document.getElementById("three").addEventListener("click",wrongnode)
+            document.getElementById("four").addEventListener("click",correctnode)
+            break
+    }
+    if(counter == curr.length){
+        counter = 0
+        playorder()
+    }
+}
+
+function correctnode()
+{
+    beep()
+    counter +=1
+    cleanbuttons()
+    playersturn()
+}
+
+function cleanbuttons()
+{
+    var buttons = document.getElementsByClassName("simonsbutton")
+    for(var button of buttons)
+    {
+        var newbutton = button.cloneNode()
+        button.parentNode.replaceChild(newbutton,button)
+    }
+}
+
+function wrongnode()
+{
+    if(lives == 1){
+        stopgame()
+        return
+    }
+    lives -=1
+    var liveselements = document.getElementById("lives").children
+    document.getElementById("lives").removeChild(liveselements[0])
+
+}
